55020ae1ac4c15350d2ba11001e2344db039e647
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2026-05-27 21:29:36 +0200

    First test to penality avoidance

--- src/belief.js
+++ src/belief.js
@@ -178,6 +178,10 @@ export class Beliefs {
     this.#plannerBeliefSet = new Beliefset();
   }
 
+  get tileWithCrateMap() {
+    return this.#tileWithCrateMap;
+  }
+
   get tileMap() {
     return this.#tileMap;
   }
@@ -568,8 +572,8 @@ export class Beliefs {
   }
 
   /**
-   * @param {Coordinates} tileCoordinates 
-   */
+  * @param { Coordinates } tileCoordinates
+  */
   isTileWithCrate(tileCoordinates) {
     for (const [_, tile] of this.#tileWithCrateMap) {
       if (tileCoordinates.x == tile.x && tileCoordinates.y == tile.y) {
